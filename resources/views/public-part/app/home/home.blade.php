e012ba3808c9115d0d46e7642674eca548443341
Edin Kahvedžić <113706715+edinkahvedzic@users.noreply.github.com>
2024-03-28 20:56:54 +0100

    how to apply section

--- resources/views/public-part/app/home/home.blade.php
+++ resources/views/public-part/app/home/home.blade.php
@@ -127,4 +127,22 @@
             </div>
         </div>
     </div>
+    <div class="how-to-apply">
+        <div class="how-to-apply__container">
+            <h2 class="how-to-apply__heading">Kako aplicirati?</h2>
+            <div class="how-to-apply__step">
+                <img src="/files/images/public-part/how-to-apply.svg" alt="How to apply image" class="how-to-apply__step-image">
+                <div class="how-to-apply__step-content">
+                    <h3 class="how-to-apply__step-heading">Upis na akademiju</h3>
+                    <p class="how-to-apply__step-text">Edukativni program Helem Nejse talent akademije naglašava i važnost kritičkog mišljenja za relevantno i seriozno djelovanje u kreativnim industrijama današnjice. Angažovani rad i kritičko razmišljanje je šesti program Akademije i jedini je program akademije koji prate svi polaznici svih odsjeka akademije.</p>
+                </div>
+            </div>
+            <div class="how-to-apply__pagination">
+                <div class="how-to-apply__pagination-item how-to-apply__pagination-item--active">1</div>
+                <div class="how-to-apply__pagination-item">2</div>
+                <div class="how-to-apply__pagination-item">3</div>
+                <div class="how-to-apply__pagination-item">4</div>
+            </div>
+        </div>
+    </div>
 @endsection
