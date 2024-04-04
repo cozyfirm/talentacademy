ea9b6671bdba3c811aa985712e23e382fafd6346
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-04-04 18:57:27 +0200

    Menu .. some changes ..

--- resources/views/public-part/layout/snippets/menu.blade.php
+++ resources/views/public-part/layout/snippets/menu.blade.php
@@ -11,4 +11,49 @@
             <img src="/files/images/public-part/mobile-menu-button.svg" alt="Mobile menu button">
         </div>
     </div>
+
+    <div class="header__links">
+        <a href="#">{{ __('Programi') }}</a>
+        <a href="#">{{ __('Predavači') }}</a>
+        <a href="#">{{ __('Stipendije') }}</a>
+        <a href="#">{{ __('Blog') }}</a>
+        <a href="#">{{ __('Lokacije') }}</a>
+        <a href="#">
+            {{ __('Programi akademije') }}
+            <i class="fas fa-chevron-down"></i>
+        </a>
+    </div>
+
+    <div class="header__submenu">
+        <a href="">
+            <div class="header__submenu_elem">
+                <img src="{{ asset('files/images/public-part/programs-1.svg') }}" alt="">
+                <p>{{ __('Novinarstvo i društvene mreže') }}</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="header__submenu_elem">
+                <img src="{{ asset('files/images/public-part/programs-1.svg') }}" alt="">
+                <p>{{ __('Pisanje za 21. stoljeće') }}</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="header__submenu_elem">
+                <img src="{{ asset('files/images/public-part/programs-1.svg') }}" alt="">
+                <p>{{ __('Odgovorno kodiranje i Civic Tech') }}</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="header__submenu_elem">
+                <img src="{{ asset('files/images/public-part/programs-1.svg') }}" alt="">
+                <p>{{ __('Grafički dizajn animacija') }}</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="header__submenu_elem">
+                <img src="{{ asset('files/images/public-part/programs-1.svg') }}" alt="">
+                <p>{{ __('Primijenjena muzička produkcija') }}</p>
+            </div>
+        </a>
+    </div>
 </header>
