2b7c02798d22008e2cd873e3da1ef8221cd63372
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-04-06 16:30:11 +0200

    Presenters ...

---
+++ resources/views/public-part/app/base-includes/presenters_scroll.blade.php
@@ -0,0 +1,115 @@
+<!-- Presenters -->
+<div class="presenters__scrollbar">
+    <div class="generic__header">
+        <h2>{{ __('Predavači') }}</h2>
+        <button class="gh_button">{{ __('Svi predavači') }}</button>
+    </div>
+
+    <div class="presenters__scrollbar_w slider_w">
+        <div class="single__presenter item1">
+            <div class="sp__image">
+                <img src="{{ asset('files/images/presenters/ana_krstajic.png') }}" alt="">
+            </div>
+            <div class="sp__btn_w">
+                <button class="sp__btn">{{ __('Keynote speaker') }}</button>
+            </div>
+            <div class="sp__text_w">
+                <h1>Ana Krstajić</h1>
+                <h2>Barklee</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+            <div class="sp__icons_w">
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/in.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/twitter.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/basket.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="single__presenter item2">
+            <div class="sp__image">
+                <img src="{{ asset('files/images/presenters/blanka.png') }}" alt="">
+            </div>
+            <div class="sp__btn_w">
+                <button class="sp__btn">{{ __('Keynote speaker') }}</button>
+            </div>
+            <div class="sp__text_w">
+                <h1>Ana Krstajić</h1>
+                <h2>Barklee</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+            <div class="sp__icons_w">
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/in.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/twitter.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/basket.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="single__presenter item3">
+            <div class="sp__image">
+                <img src="{{ asset('files/images/presenters/ana_krstajic.png') }}" alt="">
+            </div>
+            <div class="sp__btn_w">
+                <button class="sp__btn">{{ __('Keynote speaker') }}</button>
+            </div>
+            <div class="sp__text_w">
+                <h1>Ana Krstajić</h1>
+                <h2>Barklee</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+            <div class="sp__icons_w">
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/in.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/twitter.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/basket.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="single__presenter item4">
+            <div class="sp__image">
+                <img src="{{ asset('files/images/presenters/blanka.png') }}" alt="">
+            </div>
+            <div class="sp__btn_w">
+                <button class="sp__btn">{{ __('Keynote speaker') }}</button>
+            </div>
+            <div class="sp__text_w">
+                <h1>Ana Krstajić</h1>
+                <h2>Barklee</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
+            <div class="sp__icons_w">
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/in.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/twitter.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ asset('files/images/public-part/basket.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="presenters__scrollbar_btn_w">
+        <div class="p__sbw_btn">
+            <img src="{{ asset('files/images/public-part/arrow-left.png') }}" alt="">
+        </div>
+        <div class="p__sbw_btn">
+            <img src="{{ asset('files/images/public-part/arrow-right.png') }}" alt="">
+        </div>
+    </div>
+</div>
