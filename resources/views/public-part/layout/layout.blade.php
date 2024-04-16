ecc9e6a18138e2fe2d2c3998904ae7e13220dac8
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-04-16 18:50:19 +0200

    #

--- resources/views/public-part/layout/layout.blade.php
+++ resources/views/public-part/layout/layout.blade.php
@@ -5,7 +5,8 @@
         <script src="https://kit.fontawesome.com/cdf2a0a58b.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://use.typekit.net/uyb8hzd.css">
+        <link href="//fastly-cloud.typenetwork.com/projects/7921/fontface.css?660e9b3f" rel="stylesheet" type="text/css">
+{{--        <link rel="stylesheet" href="https://use.typekit.net/uyb8hzd.css">--}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
         @vite(['resources/css/public-part/layout.scss', 'resources/js/app.js'])
