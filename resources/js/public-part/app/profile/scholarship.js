f19cc4c232a4f795cdf89b1590a1669e96dfb693
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-04-20 13:07:54 +0200

    Apply for scholarship; Edit contstants

--- resources/js/public-part/app/profile/scholarship.js
+++ resources/js/public-part/app/profile/scholarship.js
@@ -1,12 +1,23 @@
 $(document).ready(function (){
     let currentID = 1;
 
+    let changeColor = function (){
+        let wrapper = $(".programs__wrapper_colored");
+        for(let i=1; i<=5; i++){
+            wrapper.removeClass('programs__wrapper_colored_' + i);
+        }
+
+        wrapper.addClass('programs__wrapper_colored_' + currentID);
+    }
+
     $(".apply-for-scholarship-next").click(function (){
         currentID ++;
         if(currentID > 5) currentID = 1;
 
         $(".single__program_wrapper").css('display', 'none');
         $(".single__program_wrapper-" + currentID).css('display', 'inline-flex');
+
+        changeColor();
     });
 
     $(".apply-for-scholarship-previous").click(function (){
@@ -15,5 +26,7 @@ $(document).ready(function (){
 
         $(".single__program_wrapper").css('display', 'none');
         $(".single__program_wrapper-" + currentID).css('display', 'inline-flex');
+
+        changeColor();
     });
 });
