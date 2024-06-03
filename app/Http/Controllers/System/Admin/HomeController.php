bff0bf0bbbdead426f7306565469995c5b660838
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2024-06-03 23:45:48 +0200

    #

--- app/Http/Controllers/System/Admin/HomeController.php
+++ app/Http/Controllers/System/Admin/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\System\Admin;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
+    public function index(){
+        dump(Carbon::now() > Carbon::parse('2024-06-04 01:00:00'), Carbon::now(), Carbon::parse('2024-06-04 01:00:00'));
+
         return view('admin.home');
     }
 }
