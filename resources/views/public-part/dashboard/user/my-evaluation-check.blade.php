fa0510e5efb8ae8c36321ed0f93551de8cab86ea
Aladin Kapić <aladin.kapic@bstelecom.ba>
2024-08-05 12:15:38 +0200

    Preview session

--- resources/views/public-part/dashboard/user/my-evaluation-check.blade.php
+++ resources/views/public-part/dashboard/user/my-evaluation-check.blade.php
@@ -17,12 +17,12 @@
                         <div class="single__evaluation single__evaluation_1">
                             <div class="session__w">
                                 <div class="session_name__w">
-                                    <p>Audio miksanje</p>
+                                    <p>{{ $session->title ?? '' }}</p>
                                 </div>
 
                                 <div class="presenter__w">
                                     <img src="{{ asset('files/images/svg-icons/speaker.svg') }}" alt="IG icon">
-                                    <p>Šemsa Suljaković</p>
+                                    <p>{{ $session->presenterRel->name ?? '' }}</p>
                                 </div>
                             </div>
                             <div class="evaluation__btn__w">
