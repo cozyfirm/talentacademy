fa8a2a656c0f3d9ba1fd8f3966a4a48a0fe07574
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2025-05-10 13:11:23 +0200

    Show only active locations

--- app/Http/Controllers/PublicPart/LocationsController.php
+++ app/Http/Controllers/PublicPart/LocationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\PublicPart;
 
 use App\Http\Controllers\Controller;
 use App\Models\Other\Location;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -13,21 +14,21 @@ class LocationsController extends Controller{
 
     public function locations(): View{
         if(Auth::check()){
-            $locations = Location::get();
+            $locations = Location::where('active', '=', 1)->get();
         }else{
-            $locations = Location::where('public', '=', 1)->get();
+            $locations = Location::where('active', '=', 1)->where('public', '=', 1)->get();
         }
         return view($this->_path . 'locations', [
             'locations' => $locations
         ]);
     }
 
-    public function single_location($id): View{
+    public function single_location($id): View | RedirectResponse{
         if(Auth::check()){
-            $locations = Location::inRandomOrder()->take(6)->get();
+            $locations = Location::where('active', '=', 1)->inRandomOrder()->take(6)->get();
             $location  = Location::where('id', '=', $id)->first();
         }else{
-            $locations = Location::where('public', '=', 1)->inRandomOrder()->take(6)->get();
+            $locations = Location::where('active', '=', 1)->where('public', '=', 1)->inRandomOrder()->take(6)->get();
 
             $location  = Location::where('id', '=', $id)->where('public', '=', 1)->first();
             if(!$location) return back();
