0dbd9511b0f155d01921a254ca027b3be2ff186c
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2025-05-15 14:27:15 +0200

    HNTA Share

--- resources/views/public-part/layout/snippets/meta-tags.blade.php
+++ resources/views/public-part/layout/snippets/meta-tags.blade.php
@@ -7,13 +7,13 @@
 <meta property="og:url" content="@yield('meta_uri', 'https://talentakademija.ba')">
 <meta property="og:title" content="@yield('meta_title', __('Helem Nejse Talent akademija'))">
 <meta property="og:description" content="@yield('meta_desc', __('Sedmodnevna ljetna škola iz oblasti kreativnih industrija u Sarajevu!'))">
-<meta property="og:image" content="@yield('meta_img', asset('files/images/public-part/HNTAShare.png'))">
-<meta property="og:image:secure_url" content="@yield('meta_img', asset('files/images/public-part/HNTAShare.png'))">
+<meta property="og:image" content="@yield('meta_img', asset('files/images/public-part/HntaShare2025.png'))">
+<meta property="og:image:secure_url" content="@yield('meta_img', asset('files/images/public-part/HntaShare2025.png'))">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="@yield('meta_uri', 'https://talentakademija.ba')">
 <meta property="twitter:title" content="@yield('meta_title', __('Helem Nejse Talent akademija'))">
 <meta property="twitter:description" content="@yield('meta_desc', __('Sedmodnevna ljetna škola iz oblasti kreativnih industrija u Sarajevu!'))">
-<meta property="twitter:image" content="@yield('meta_img', asset('files/images/public-part/HNTAShare.png'))">
-<meta property="twitter:image:secure_url" content="@yield('meta_img', asset('files/images/public-part/HNTAShare.png'))">
+<meta property="twitter:image" content="@yield('meta_img', asset('files/images/public-part/HntaShare2025.png'))">
+<meta property="twitter:image:secure_url" content="@yield('meta_img', asset('files/images/public-part/HntaShare2025.png'))">
